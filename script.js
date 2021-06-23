87e6611fb74ed8604110eb2b10854a9aeb420eef
Xander <xandersmith2008@gmail.com>
2021-06-23 20:47:20 +0100

    landing page!

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-$(document).ready(function() {
-    const url = window.location.href;
-    let doDownload = getURLParameter(url, 'download');
-    if (doDownload === "1") download();
+const logo = $('#logo');
+$('body').mousemove(function (e) {
+    if (e.pageX > logo.position().left + (logo.width() / 2)) {
+        logo.attr('src', './resources/right.png');
+    } else {
+        logo.attr('src', './resources/left.png');
+    }
 });
-
-
-function getURLParameter(url, name) {
-    return (RegExp(name + '=' + '(.+?)(&|$)').exec(url)||[undefined,null])[1];
-}
-
-function download() {
-    let os = "Unknown OS";
-    if(navigator.appVersion.indexOf("Win")!==-1) os = "windows";
-    if (navigator.appVersion.indexOf("Mac")!==-1) os = "mac";
-    if (navigator.appVersion.indexOf("Linux")!==-1) os = "linux";
-
-    os = os + ".txt";
-    window.location.href = "https://github.com/XanderClient/Launcher/releases/latest/download/" + os;
-}
-
-AOS.init();
-
-particlesJS.load('canvas', 'particles.json');
-
-update = function() {
-    requestAnimationFrame(update);
-}
-requestAnimationFrame(update);
