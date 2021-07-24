ed078d6d444243de4a1f733a0def7052e2a01113
Xander <xandersmith2008@gmail.com>
2021-07-24 20:11:53 +0100

    new website what...?>???

--- script.js
+++
@@ -1,8 +0,0 @@
-const logo = $('#logo');
-$('body').mousemove(function (e) {
-    if (e.pageX > logo.position().left + (logo.width() / 2)) {
-        logo.attr('src', './resources/right.png');
-    } else {
-        logo.attr('src', './resources/left.png');
-    }
-});
